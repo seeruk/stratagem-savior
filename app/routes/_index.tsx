46100631dbe1b698f8ff70078958a69b9a4f6bf4
Elliot Wright <elliot@Elliots-MacBook-Pro.local>
2024-03-30 14:51:53 +0000

    Initial app, with formatting

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,35 +1,68 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { MetaFunction } from "@remix-run/cloudflare"
+import { useCallback, useEffect } from "react"
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Subterfuge Savior" },
     {
       name: "description",
-      content: "Welcome to Remix! Using Vite and Cloudflare!",
+      content: "A shameless Stratagem Hero clone, with some extra fun modes!",
     },
-  ];
-};
+  ]
+}
+
+// TODO: Move me
+const keyToDirection = (key: string) => {
+  switch (key) {
+    case "ArrowUp":
+    case "w":
+      return "up"
+    case "ArrowDown":
+    case "s":
+      return "down"
+    case "ArrowLeft":
+    case "a":
+      return "left"
+    case "ArrowRight":
+    case "d":
+      return "right"
+    default:
+      return ""
+  }
+}
+
+// TODO: Not hard-coded
+const sequence = ["up", "down", "down", "up"]
+
+const directionSymbols = {
+  up: "⬆",
+  down: "⬇",
+  left: "⬅",
+  right: "⮕",
+}
 
 export default function Index() {
+  const onKeyDown = useCallback((evt: KeyboardEvent) => {
+    console.log(keyToDirection(evt.key))
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown, false)
+    return () => document.removeEventListener("keydown", onKeyDown, false)
+  }, [onKeyDown])
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <div
+        style={{
+          color: "white",
+          background: "black",
+          fontWeight: "bold",
+          fontSize: "64px",
+        }}
+      >
+        &#x2B05; &#x2B95; &#x2B06; &#x2B07;
+      </div>
     </div>
-  );
+  )
 }
