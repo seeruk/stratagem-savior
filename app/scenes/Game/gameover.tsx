65ee8de296bb0dbc8b5da57d56fb74cf29246eaf
Elliot Wright <elliot@Elliots-MacBook-Pro.local>
2024-03-31 16:48:13 +0100

    Added high score persistance in localStorage

--- app/scenes/Game/gameover.tsx
+++ app/scenes/Game/gameover.tsx
@@ -1,11 +1,22 @@
+import { useLocalStorage } from "@uidotdev/usehooks"
+
 import { Heading } from "~/components/Heading"
 import { Message } from "~/components/Message"
+import { useEffect } from "react"
 
 export type GameOverProps = {
   score: number
 }
 
 export function GameOver({ score }: GameOverProps) {
+  const [highScore, setHighScore] = useLocalStorage("ssgHighScore", 0)
+
+  useEffect(() => {
+    if (score > highScore) {
+      setHighScore(score)
+    }
+  }, [highScore, score, setHighScore])
+
   return (
     <div className="flex flex-col items-center">
       <Heading>Game Over</Heading>
@@ -13,7 +24,7 @@ export function GameOver({ score }: GameOverProps) {
       <Heading className="mb-0" level={3} size="xs">
         High Score
       </Heading>
-      <div className="mb-8 text-xl font-bold">TODO</div>
+      <div className="mb-8 text-xl font-bold">{highScore}</div>
 
       <Heading className="mb-0" level={3} size="xs">
         Your Final Score
