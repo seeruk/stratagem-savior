3642c189f636fa4415905d74f2d133af86df79a6
Elliot Wright <elliot@Elliots-MacBook-Pro.local>
2024-04-02 18:06:59 +0100

    Game over screen redesign, with end-of-mission messaging, probably to be tweaked...

--- app/scenes/Game/game-over.tsx
+++ app/scenes/Game/game-over.tsx
@@ -1,8 +1,8 @@
+import { useEffect } from "react"
 import { useLocalStorage } from "@uidotdev/usehooks"
 
 import { Heading } from "~/components/Heading"
 import { Message } from "~/components/Message"
-import { useEffect } from "react"
 
 export type GameOverProps = {
   score: number
@@ -11,6 +11,15 @@ export type GameOverProps = {
 export function GameOver({ score }: GameOverProps) {
   const [highScore, setHighScore] = useLocalStorage("ssgHighScoreV1", 0)
 
+  let message = "Outstanding Patriotism"
+  if (score < 1000) {
+    message = "Disappointing Service"
+  } else if (score < 1500) {
+    message = "Underwhelming Performance"
+  } else if (score < 2000) {
+    message = "Honorable Duty"
+  }
+
   useEffect(() => {
     if (score > highScore) {
       setHighScore(score)
@@ -18,18 +27,28 @@ export function GameOver({ score }: GameOverProps) {
   }, [highScore, score, setHighScore])
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center w-[500px]">
       <Heading>Game Over</Heading>
 
-      <Heading className="mb-0" level={3} size="xs">
-        High Score
-      </Heading>
-      <div className="mb-8 text-xl font-bold">{highScore}</div>
+      <div className="flex w-[350px] justify-between">
+        <div>
+          <Heading className="mb-0" level={3} size="xs">
+            Final Score
+          </Heading>
+          <Message className="text-3xl font-bold">{score}</Message>
+        </div>
 
-      <Heading className="mb-0" level={3} size="xs">
-        Your Final Score
-      </Heading>
-      <Message className="text-3xl font-bold">{score}</Message>
+        <div className="text-right">
+          <Heading className="mb-0" level={3} size="xs">
+            High Score
+          </Heading>
+          <div className="mb-8 text-3xl font-bold">{highScore}</div>
+        </div>
+      </div>
+
+      <div className="mb-6 p-6 w-full font-bold text-black text-xl text-center bg-yellow-300">
+        {message}
+      </div>
     </div>
   )
 }
