b0f9d896366aaadde16f36aef672e6ff86da22e3
Elliot Wright <elliot@Elliots-MacBook-Pro.local>
2024-04-02 17:19:23 +0100

    Removed high score from main menu

--- app/scenes/Menu/index.tsx
+++ app/scenes/Menu/index.tsx
@@ -1,23 +1,11 @@
 import { useCallback, useEffect } from "react"
-import { ClientOnly } from "remix-utils/client-only"
 
 import { Heading } from "~/components/Heading"
 import { Message } from "~/components/Message"
 import { keyToDirection } from "~/utils"
-import { useLocalStorage } from "@uidotdev/usehooks"
 
 export type MenuProps = {
   onStart: () => void
-}
-
-const HighScore = () => {
-  const [highScore] = useLocalStorage("ssgHighScoreV1", 0)
-
-  return highScore > 0 ? (
-    <div className="text-sm mt-6 text-gray-500">
-      Your High Score: <span className="font-bold text-gray-400">{highScore}</span>
-    </div>
-  ) : null
 }
 
 export function Menu({ onStart }: MenuProps) {
@@ -42,8 +30,6 @@ export function Menu({ onStart }: MenuProps) {
       <Heading>Stratagem Savior</Heading>
       <Message className="mb-1 font-bold">Press any Stratagem Input to Start!</Message>
       <div className="text-xs text-gray-500">(WASD or arrow keys)</div>
-
-      <ClientOnly>{() => <HighScore />}</ClientOnly>
     </div>
   )
 }
